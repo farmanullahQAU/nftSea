81aa273422fa573894812274d01499a79fe17058
farmanullahQAU <farmanullahk437@gmail.com>
2024-03-01 23:27:22 +0500

    last update

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
- 
- 
- pragma solidity ^0.8.20;
+
+pragma solidity ^0.8.20;
 import {ERC721URIStorageUpgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/extensions/ERC721URIStorageUpgradeable.sol";
 
 import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
@@ -11,156 +10,126 @@ import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/Own
 import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "hardhat/console.sol";
 
-
 // import {ERC721Upgradeable} from "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 
-contract NFTSS is Initializable,ERC721Upgradeable,ERC721URIStorageUpgradeable{ 
-mapping(uint=>NFT) public listedTokens;
+contract NFTSS is
+    Initializable,
+    ERC721Upgradeable,
+    ERC721URIStorageUpgradeable
+{
+    mapping(uint => NFT) public listedTokens;
+    mapping(uint => mapping(address => NFT)) usersData;
 
-uint _totalItemSold;
+    uint _totalItemSold;
 
-event Added(NFT);
+    event Added(NFT);
 
- uint256 private _nextTokenId;
+    uint256 private _nextTokenId;
 
-   uint256 charges; //0.0001 ehter ; //listing charges by marketplace
+    uint256 charges; //0.0001 ehter ; //listing charges by marketplace
 
-function initialize() public initializer {
-
+    function initialize() public initializer {
         __ERC721_init("MyToken", "MTK");
 
-   charges=100000000000000 wei; //0.0001 ehter ; //listing charges by marketplace
-
-    
-        
+        charges = 100000000000000 wei; //0.0001 ehter ; //listing charges by marketplace
     }
 
-//  constructor() initializer {}
-//   constructor() {
-//         _disableInitializers();
-//     }
+    struct NFT {
+        address creator;
+        address owner;
+        uint price;
+        string uri;
+        bool isSold;
+        uint tokenId;
+    }
 
-//     function initialize() initializer public {
-//         __ERC721_init("MyToken", "MTK");
+    function createNFT(string memory uri, uint price) public payable {
+        require(price > charges, "Price should be greater than listing price");
 
-//   charges=100000000000000 wei; //0.0001 ehter ; //listing charges by marketplace
-
-//     }
-
-
-  struct NFT{
-    address creator;
-address owner;
-uint price;
-string uri;
-bool isSold;
-
-uint tokenId;
-  }
-
-
-
-
-
-
-
-
-  
-
-      function createNFT(string memory uri,uint price) public payable{
-
-
-    
-
-      require(price>charges,"Price should be greater than listing price");
-    
         uint256 tokenId = _nextTokenId++;
-     
-        _mint(address(this), tokenId);  //use _safeMint if we keep ownership to msg.sender
+
+        _mint(address(this), tokenId); //use _safeMint if we keep ownership to msg.sender
         _setTokenURI(tokenId, uri);
-
-
-
-        _createMarketItem(uri,price,tokenId);
+        _createMarketItem(uri, price, tokenId);
     }
 
-function _createMarketItem( string memory uri,uint price,uint tokenId) public   {
+    function _createMarketItem(
+        string memory uri,
+        uint price,
+        uint tokenId
+    ) internal {
+        listedTokens[tokenId] = NFT(
+            address(msg.sender),
+            address(this),
+            price,
+            uri,
+            false,
+            tokenId
+        );
 
-           listedTokens[tokenId]=NFT(address(msg.sender),address(this),
-          price,uri,false,tokenId);
+        emit Added(listedTokens[tokenId]);
+    }
 
-emit Added(listedTokens[tokenId]);
+    // function sellNFT(uint tokenId)public payable {
+    // require(listedTokens[tokenId].isSold==false,"Already sold");
+    // uint price=listedTokens[tokenId].price *1 ether;
+    // address seller=listedTokens[tokenId].creator;
+    // require(msg.value==price ,"Please submit the asking price in order to continue");
 
-}
+    // _transfer(address(this), msg.sender, tokenId); //transfer ownership of nft from market to buyer
+    // payable(address(this)).transfer(charges);// deduct listing chrges from seller
 
+    // payable(seller).transfer(msg.value); //transfer amount to seller
+    // listedTokens[tokenId].isSold=true;
+    // _totalItemSold++;
 
-// function sellNFT(uint tokenId)public payable {
-// require(listedTokens[tokenId].isSold==false,"Already sold");
-// uint price=listedTokens[tokenId].price *1 ether;
-// address seller=listedTokens[tokenId].creator;
-// require(msg.value==price ,"Please submit the asking price in order to continue");
+    // }
 
-// _transfer(address(this), msg.sender, tokenId); //transfer ownership of nft from market to buyer
-// payable(address(this)).transfer(charges);// deduct listing chrges from seller
+    function buyNft(uint tokenId) public payable {
+        // Check if NFT is already sold
+        require(listedTokens[tokenId].isSold == false, "NFT already sold");
+        require(msg.value > charges, "Isufficient balance");
 
-// payable(seller).transfer(msg.value); //transfer amount to seller
-// listedTokens[tokenId].isSold=true;
-// _totalItemSold++;
+        // Get price and seller address
+        uint price = listedTokens[tokenId].price;
+        address seller = listedTokens[tokenId].creator;
 
-// }
+        // Ensure buyer sent correct price
+        require(
+            msg.value == price,
+            "Please send the exact asking price to proceed"
+        );
 
-function buyNft(uint tokenId) public payable {
-  // Check if NFT is already sold
-  require(listedTokens[tokenId].isSold == false, "NFT already sold");
-  require(msg.value>charges,"Isufficient balance");
-
-  // Get price and seller address
-  uint price = listedTokens[tokenId].price;
-  address seller = listedTokens[tokenId].creator;
-
-  // Ensure buyer sent correct price
-  require(msg.value == price , "Please send the exact asking price to proceed");
-
-  // Transfer NFT ownership to buyer (let buyer pay gas)
-  _transfer(address(this), msg.sender, tokenId);
-
-  // Deduct listing charges from seller payment
-  
-   // Send remaining payment to seller after deducting charges
-  uint sellerPayment = msg.value - charges;
-  payable(seller).transfer(sellerPayment);
-  // Update NFT status and counter
-  listedTokens[tokenId].isSold = true;
-
-
-  _totalItemSold++;
-}
+        // Transfer NFT ownership to buyer (let buyer pay gas)
+        _transfer(address(this), msg.sender, tokenId);
 
 
 
+        // Deduct listing charges from seller payment
+
+        // Send remaining payment to seller after deducting charges
+        uint sellerPayment = msg.value - charges;
+        payable(seller).transfer(sellerPayment);
+        // Update NFT status and counter
+        listedTokens[tokenId].isSold = true;
+        listedTokens[tokenId].owner=msg.sender;
 
 
 
+        _totalItemSold++;
+    }
 
-function totalNFTs() public view returns (NFT[] memory) {
-
-
-uint total=balanceOf(address(this));
-
-console.log(total);
-    NFT[] memory nfts = new NFT[](total);
-    for (uint i = 0; i < total; i++) {
-     
+    function totalNFTs() public view returns (NFT[] memory) {
+        NFT[] memory nfts = new NFT[](_nextTokenId);
+        for (uint i = 0; i < nfts.length; i++) {
             nfts[i] = listedTokens[i];
-        
+        }
+        return nfts;
     }
-    return nfts;
-}
 
-
-
-
-function tokenURI(uint256 tokenId)
+    function tokenURI(
+        uint256 tokenId
+    )
         public
         view
         override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
@@ -169,7 +138,9 @@ function tokenURI(uint256 tokenId)
         return super.tokenURI(tokenId);
     }
 
-    function supportsInterface(bytes4 interfaceId)
+    function supportsInterface(
+        bytes4 interfaceId
+    )
         public
         view
         override(ERC721Upgradeable, ERC721URIStorageUpgradeable)
@@ -177,6 +148,4 @@ function tokenURI(uint256 tokenId)
     {
         return super.supportsInterface(interfaceId);
     }
-
-
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,21 +1,4 @@
 
-// const hre = require("hardhat");
-
-// async function main() {
-
-
-//   const lock = await hre.ethers.deployContract("NFTsea");
-
-//   await lock.waitForDeployment();
-
-//   console.log(`NFTsea deployed to ${lock.target}`);
-// }
-
-// main().catch((error) => {
-//   console.error(error);
-//   process.exitCode = 1;
-// });
-
 
 // scripts/deploy_upgradeable_box.js
 require('@openzeppelin/hardhat-upgrades');
